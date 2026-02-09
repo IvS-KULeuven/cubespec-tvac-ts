f7b3d7eecf07cd2e1e10ace0c7843f1f6e8145c1
Sara Regibo <sara@ster.kuleuven.be>
2026-02-09 16:46:09 +0100

    TVAC UI
    Configuration of the heaters (+ corresponding PSUs).

---
+++ src/tvac/tasks/tvac/__init__.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+import sys
+from executor import ExternalCommand
+
+HERE = Path(__file__).parent.resolve()
+
+UI_TAB_ORDER = ["heaters"]
+
+
+def tvac_ui():
+    logo_path = HERE / "icons/dashboard.svg"
+
+    cmd_log = os.environ.get("CUBESPEC_LOG_FILE_LOCATION")
+    if cmd_log is None or not os.access(cmd_log, os.W_OK):
+        cmd_log = str(Path("~").expanduser())
+
+    cmd = ExternalCommand(
+        f"gui-executor --verbose --module-path tvac.tasks.tvac.heaters "
+        f"--logo {logo_path} --cmd-log {cmd_log} --app-name 'TVAC GUI' "
+        f"{' '.join(sys.argv[1:])}",
+        asynchronous=True,
+    )
+    cmd.start()
---
+++ src/tvac/tasks/tvac/heaters/heaters.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from gui_executor.exec import exec_ui
+from gui_executor.utypes import Callback
+
+from tvac.heaters import print_heater_settings
+from tvac.tasks.tvac.heaters import heaters
+
+UI_MODULE_DISPLAY_NAME = "2 - Heaters"
+HERE = Path(__file__).parent.parent.resolve()
+ICON_PATH = HERE / "icons/"
+
+
+@exec_ui(display_name="Heater settings", use_kernel=True)
+def get_heater_settings(heater: Callback(heaters, name="Heater") = None) -> None:
+    """Prints the settings for the given heater.
+
+    Args:
+        heater (Callback | None): Name of the heater.
+    """
+
+    print_heater_settings(heater_name=heater)
