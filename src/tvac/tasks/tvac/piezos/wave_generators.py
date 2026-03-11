8d231e2ef9948fcf7311b3abdaaed3e2444cbab7
Sara Regibo <sara@ster.kuleuven.be>
2026-03-11 10:07:48 +0100

    Tasks for the piezos

--- src/tvac/tasks/tvac/piezos/wave_generators.py
+++ src/tvac/tasks/tvac/piezos/wave_generators.py
@@ -12,33 +12,37 @@ HERE = Path(__file__).parent.parent.resolve()
 ICON_PATH = HERE / "icons/"
 
 
-@exec_ui(display_name="Test", use_kernel=True)
-def print_test():
-    print("Test")
+@exec_ui(display_name="Configuration & switch-on", use_kernel=True)
+def switch_on_piezos(
+    profile: Callback(profiles, name="Voltage profile") = None,
+) -> None:
+    """Configures and switches on the Wave Generators for the given voltage profile.
+
+    Args:
+        profile: Voltage profile.
+    """
+
+    start_observation("Configure + switch on wave generators, using profile {profile}")
+
+    try:
+        config_awg(profile=profile)
+    except Exception as e:
+        print(
+            f"Failed to configure + switch on wave generators, using profile {profile}: {e}"
+        )
+
+    end_observation()
 
 
-# @exec_ui(display_name="Configuration & switch-on", use_kernel=True)
-# def switch_on_piezos(profile: Callback(profiles, name="Voltage profile") = None
-# ) -> None:
-#     """Configures and switches on the Wave Generators for the given voltage profile.
-#
-#     Args:
-#         profile: Voltage profile.
-#     """
-#
-#     start_observation("Configure + switch on wave generators, using profile {profile}")
-#
-#     try:
-#         config_awg(profile=profile)
-#     except Exception as e:
-#         print(f"Failed to configure + switch on wave generators, using profile {profile}: {e}")
-#
-#     end_observation()
-#
-#
-# @exec_ui(display_name="Switch-off", use_kernel=True)
-# def switch_off_piezos(
-# ) -> None:
-#     """Switches off the Wave Generators."""
-#
-#     pass
+@exec_ui(display_name="Switch-off", use_kernel=True)
+def switch_off_piezos() -> None:
+    """Switches off the Wave Generators."""
+
+    start_observation("Switch off wave generators")
+
+    try:
+        switch_off_awg(profile=profile)
+    except Exception as e:
+        print(f"Failed to switch off wave generators: {e}")
+
+    end_observation()
