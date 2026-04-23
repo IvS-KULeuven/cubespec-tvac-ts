19e49a397e3e139eee8d357fcbb2ff252170b13a
Sara Regibo <sara@ster.kuleuven.be>
2026-04-23 08:24:46 +0200

    Make absence of amplifier clear in TVAC UI
    Added red border around the UI + warning message

--- src/tvac/runtime_config.py
+++ src/tvac/runtime_config.py
@@ -11,3 +11,6 @@ def is_amplifier_excluded() -> bool:
 
 def exclude_amplifier(exclude: bool) -> None:
     os.environ[EXCLUDE_AMPLIFIER_ENV_VAR] = "1" if exclude else "0"
+
+    if exclude:
+        os.environ["GUI_EXECUTOR_ATTENTION_LABEL"] = "NO AMPLIFIER"
